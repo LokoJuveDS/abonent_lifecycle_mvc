d42a6bee6550294582372fd80cdf80619886748f
Yegor Priimak <egorpryimak@gmail.com>
2021-02-15 12:57:24 +0300

    configuration: add dependencies in pom.xml, create web.xml, applicationContext

---
+++ src/main/java/com/yegorpriimak/spring/mvc/Subscriber.java
@@ -0,0 +1,10 @@
+package com.yegorpriimak.spring.mvc;
+
+public class Subscriber {
+    private String firstName;
+    private String lastName;
+    private double msisdn;
+    private double balance;
+    private boolean status;
+
+}
---
+++ src/main/webapp/WEB-INF/view/first-view.jsp
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<h2>First View for test</h2>
+
+<br>
+<br>
+<br>
+
+<a href="askDetails">Please write your details</a>
+
+</body>
+
+</html>
